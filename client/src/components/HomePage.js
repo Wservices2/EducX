3e512a8c97da04576c4d58949b28338f6a517ba1
Wservices2 <wils2service@gmail.com>
2025-12-20 21:35:12 +0100

    Fix: Recreate BottomNavigation.js without Git conflicts

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1108,7 +1108,7 @@ const HomePage = () => {
   );
 };
 
-=======
+
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
